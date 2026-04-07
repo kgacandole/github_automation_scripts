0ed7b42fe2ba2b1fc536602de0a518073184f79e
kgacandole <kevin.candole@gmail.com>
2026-04-07 17:18:28 +0800

    added wf to list repos

---
+++ python/list_repositories.py
@@ -0,0 +1,27 @@
+import sys, requests
+
+tokenId = sys.argv[1]
+maxCounter = sys.argv[2]
+search_strings = sys.argv[3]
+skip_strings = sys.argv[4]
+ownerName = sys.argv[5]
+
+ghUrl = "https://api.github.com/orgs/" + ownerName + "/repos"
+ghHeaders = { 
+    "Authorization": "Bearer " + tokenId, 
+    "X-GitHub-Api-Version": "2022-11-28", 
+    "Accept": "application/vnd.github+json"
+}
+
+for x in range(counter):
+    url = ghUrl + "?per_page=" + maxCounter + "&page=" + str(x)
+    getRepos = requests.get(url, headers = ghHeaders).json()
+
+    for repos in getRepos:
+        reponame = repos['name']
+
+        if search_strings != "":        
+            if any(validstr in reponame for validstr in search_strings.split(",")):
+                if skip_strings != "" and not any(excemptrepo in reponame for excemptrepo in skip_strings.split(",")):
+                    with open("repo_list.txt", "a") as f:
+                        f.write(reponame + "\n")
