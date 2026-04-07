3a9685fa276e3b83b9cc7e1abecfd58472749d84
kgacandole <kevin.candole@gmail.com>
2026-04-07 18:06:38 +0800

    edited cmd execution

--- python/list_repositories.py
+++ python/list_repositories.py
@@ -1,12 +1,11 @@
 import sys, requests
 
 tokenId = sys.argv[1]
-maxCounter = int(sys.argv[2])
-search_strings = sys.argv[3]
-skip_strings = sys.argv[4]
-ownerName = sys.argv[5]
+ownerName = sys.argv[2]
+maxCounter = int(sys.argv[3])
+search_strings = sys.argv[4]
 
-ghUrl = "https://api.github.com/orgs/" + ownerName + "/repos"
+ghUrl = "https://api.github.com/users/" + ownerName + "/repos"
 ghHeaders = { 
     "Authorization": "Bearer " + tokenId, 
     "X-GitHub-Api-Version": "2026-03-10", 
@@ -14,20 +13,14 @@ ghHeaders = {
 }
 
 for x in range(maxCounter):
-    url = ghUrl + "?per_page=" + str(maxCounter) + "&page=" + str(x)
-    print(url)
+    url = ghUrl + "?per_page=" + str(maxCounter) + "&page=" + str(x + 1)
     getRepos = requests.get(url, headers = ghHeaders).json()
-    print(getRepos)
-    print(ghHeaders)
     try:
         for repos in getRepos:
-            print(repos)
             reponame = repos['name']
-
-            if search_strings != "":        
-                if any(validstr in reponame for validstr in search_strings.split(",")):
-                    if skip_strings != "" and not any(excemptrepo in reponame for excemptrepo in skip_strings.split(",")):
-                        with open("repo_list.txt", "a") as f:
-                            f.write(reponame + "\n")
+            print(reponame)
+            if any(validstr in reponame for validstr in search_strings.split(",")):                
+                with open("repo_list.txt", "a") as f:
+                    f.write(reponame + "\n")
     except Exception as e:
         raise RuntimeError("Unable to get repositories") from e
